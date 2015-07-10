3c55bf89d6acfb1e747133054e904d198ebbafe5
Steve Chung <steve.chung7@gmail.com>
2015-07-10 10:45:35 -0400

    deep_class_var

--- lib/csv_row_model/model.rb
+++ lib/csv_row_model/model.rb
@@ -1,3 +1,4 @@
+require 'csv_row_model/model/deep_class_var'
 require 'csv_row_model/model/columns'
 require 'csv_row_model/model/children'
 
@@ -10,8 +11,8 @@ module CsvRowModel
       include ActiveModel::Validations
       include Validators::ValidateAttributes
 
+      include DeepClassVar
       include Columns
-
       include Children
 
       # @return [Model] return the parent, if this instance is a child
@@ -39,43 +40,6 @@ module CsvRowModel
     # @return [Boolean] returns true, if the entire csv file should stop reading
     def abort?
       false
-    end
-
-    class_methods do
-
-      protected
-
-      # Returns a memoized variable on the class that included `included_module`
-      #
-      # @param variable_name [Symbol] name of the variable name memoized
-      # @param default_value [Symbol] default value of the memoized variable
-      # @param included_module [Module] module to search for
-      # @return [Object] returns the value of the instance variable of the class that included `included_module`
-      def memoized_class_included_var(variable_name, default_value, included_module)
-        class_included = class_included(included_module)
-        if self == class_included
-          #
-          # equal to: @variable_name ||= default_value
-          #
-          variable_name = "@#{variable_name}"
-          instance_variable_get(variable_name) || instance_variable_set(variable_name, default_value)
-        else
-          class_included.public_send(variable_name)
-        end
-      end
-
-      # Returns the class that included `included_module`, so class variables can be stored there without inheritance prpblems
-      #
-      # @param included_module [Module] module to search for
-      # @return [Class] the class that included `included_module`
-      def class_included(included_module)
-        @class_included ||= {}
-        @class_included[included_module] ||= begin
-          inherited_ancestors = ancestors[0..(ancestors.index(included_module) - 1)]
-          index = inherited_ancestors.rindex {|inherited_ancestor| inherited_ancestor.class == Class }
-          inherited_ancestors[index]
-        end
-      end
     end
   end
 end
