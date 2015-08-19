614640b6cc0a4f45d25e4a4f5b3a218c219ebbfc
Steve Chung <steve.chung7@gmail.com>
2015-08-19 08:36:42 -0400

    simplify and comment number validations

--- lib/csv_row_model/validators/integer_format.rb
+++ lib/csv_row_model/validators/integer_format.rb
@@ -1,7 +1,9 @@
 class IntegerFormatValidator < CsvRowModel::Validators::NumberValidator # :nodoc:
   def validate_each(record, attribute, value)
     before, after = before_after_decimal(value)
-    return if value.class == String && value.to_i.to_s == before && after.empty?
+
+    return if value.to_i.to_s == before && after.empty?
+
     record.errors.add(attribute, 'is not a Integer format')
   end
 end
