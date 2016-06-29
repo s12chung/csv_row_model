c530762a0e97a97687a21716a48fbd096b5975bc
Steve Chung <steve.chung7@gmail.com>
2016-06-29 14:06:58 -0400

    handle file_model change for format_header

--- spec/support/fixtures/row_models/file_row_model.rb
+++ spec/support/fixtures/row_models/file_row_model.rb
@@ -5,8 +5,8 @@ class FileRowModel
   row :string1
   row :string2, header: 'String 2'
 
-  def self.format_header(column_name, context)
-    ":: - #{column_name} - ::"
+  class << self
+    def format_header(column_name, index, context); ":: - #{column_name} - ::" end
   end
 end
 
